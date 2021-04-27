4e4c82749f18872330688b196905dd19165bb801
banfanci <bflobos@uc.cl>
2021-04-26 20:09:04 -0400

    change generic error

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use('/albums', albumRoutes);
 app.use('/tracks', trackRoutes);
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 405));
 });
 
 app.use(globalErrorHandler);
